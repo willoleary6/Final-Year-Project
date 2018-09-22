869cef40ec60d1a7adb758dffe427498f08ffaf4
William <willoleary6@gmail.com>
2018-09-22 16:22:29 +0100

    Leaving tensorflow for now... need to get a firmer grasp on how the theory works so im building another neural network based off of Michael Neilson's book, however that was writted for Python 2 so Im converting it to Python 3 which allows me to go through it line my line while also studying his book. so far the file reader and formatter we are using to break the data set into something readable for the neural network is complete

---
+++ neuralNetworksAndDeepLearningMnistClassifier/src/runner.py
@@ -0,0 +1,9 @@
+
+
+
+import mnist_data_set_loader
+
+# Before we can start training we must first retrieve the data set
+# from the data_set_loader we will populate the training data set, validation data set and the test data set
+training_data, validation_data, test_data = mnist_data_set_loader.load_data_and_shape()
+
