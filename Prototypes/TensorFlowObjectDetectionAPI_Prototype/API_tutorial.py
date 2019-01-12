9fe60b1d96019958c8a92823d5fa60ef2b782e5d
William <willoleary6@gmail.com>
2019-01-12 14:58:12 +0000

    Video input is now working and i found a way to parse photos faster, will clean up code next before progressing further

--- Prototypes/TensorFlowObjectDetectionAPI_Prototype/API_tutorial.py
+++ Prototypes/TensorFlowObjectDetectionAPI_Prototype/API_tutorial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
-import six.moves.urllib as urllib
+from os import walk
+import six.moves.urllib as url_library
 
 import tarfile
 import tensorflow as tf
@@ -21,7 +22,7 @@ if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
     raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
 # Model preparation Variables Any model exported using the export_inference_graph.py tool can be loaded here simply
-# by changing PATH_TO_FROZEN_GRAPH to point to a new .pb file.
+# by changing PATH_TO_FROZEN_GRAPH_OF_MODEL to point to a new .pb file.
 
 # By default we use an "SSD with Mobilenet" model here. See the detection model zoo for a list of other models that
 # can be run out-of-the-box with varying speeds and accuracies.
@@ -30,61 +31,76 @@ if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
 os.chdir(os.getcwd() + '\models\\research\object_detection')
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
+model_file = model_name + '.tar.gz'
+download_base = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_FROZEN_GRAPH_OF_MODEL = model_name + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.getcwd() + '\data\mscoco_label_map.pbtxt'
+path_to_labels = os.getcwd() + '\\data\\mscoco_label_map.pbtxt'
 # Download Model
 
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
+opener = url_library.request.URLopener()
+opener.retrieve(
+    download_base + model_file,  # url of the file we want to
+    model_file  # store model in this variable
+)
+
+model_tar_file = tarfile.open(model_file)
+for file in model_tar_file.getmembers():
     file_name = os.path.basename(file.name)
     if 'frozen_inference_graph.pb' in file_name:
-        tar_file.extract(file, os.getcwd())
+        model_tar_file.extract(file, os.getcwd())  # extract the desired file to the current directory
 
 # Load a (frozen) Tensorflow model into memory.
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+    object_detection_graph_definition = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH_OF_MODEL, 'rb') as file_id:
+        serialized_graph = file_id.read()
+        object_detection_graph_definition.ParseFromString(serialized_graph)
+        tf.import_graph_def(object_detection_graph_definition, name='')
 
 # Loading label map Label maps map indices to category names, so that when our convolution network predicts 5,
 # we know that this corresponds to airplane. Here we use internal utility functions, but anything that returns a
 # dictionary mapping integers to appropriate string labels would be fine
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+category_index = label_map_util.create_category_index_from_labelmap(path_to_labels, use_display_name=True)
 
 
 # Helper code
 
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
+def load_image_into_numpy_array(desired_image):
+    (image_width, image_height) = desired_image.size
+    return np.array(
+        desired_image.getdata()).reshape(
+        (image_height, image_width, 3)
+    ).astype(np.uint8)
 
+
+# get all the files in the test_images directory
+path_to_images = os.getcwd() + '\\test_images'
+files_in_directory = []
+for (directory_path, directory_names, file_names) in walk(path_to_images):
+    files_in_directory.extend(file_names)
+    break
+
+test_image_paths = []
+valid_file_extension = ['.jpg', '.png']
+# filter out the non image related files and build a list of file paths to each of the images
+for i in files_in_directory:
+    for j in valid_file_extension:
+        if i.endswith(j):
+            test_image_paths.append(os.path.join(path_to_images,i))
+            break
 
 # Detection
 
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = os.getcwd() + '\\test_images'
-TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 4)]
-
 # Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
+image_size = (12, 8)
 
 
 def run_inference_for_single_image(image, graph):
@@ -123,7 +139,6 @@ def run_inference_for_single_image(image, graph):
             # Run inference
             output_dict = sess.run(tensor_dict,
                                    feed_dict={image_tensor: np.expand_dims(image, 0)})
-
             # all outputs are float32 numpy arrays, so convert types as appropriate
             output_dict['num_detections'] = int(output_dict['num_detections'][0])
             output_dict['detection_classes'] = output_dict[
@@ -136,7 +151,7 @@ def run_inference_for_single_image(image, graph):
 
 
 plt.switch_backend('TKAgg')
-for image_path in TEST_IMAGE_PATHS:
+for image_path in test_image_paths:
     image = Image.open(image_path)
     # the array based representation of the image will be used later in order to prepare the
     # result image with boxes and labels on it.
@@ -158,4 +173,4 @@ for image_path in TEST_IMAGE_PATHS:
 
     plt.gcf().clear()
     plt.imshow(image_np)
-    plt.show()
+    #plt.show()
