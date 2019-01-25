26fffb40b101858b2cdc499cc075d29c1057775a
William <willoleary6@gmail.com>
2019-01-25 19:29:04 +0000

    restructuring

--- Prototypes/TensorFlowObjectDetectionAPI_Prototype/cmd_commands_to_initialise_api.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd C:/SourceCode/Final-Year-Project/Prototypes/TensorFlowObjectDetectionAPI_Prototype/models/research
-set PYTHONPATH=C:/SourceCode/Final-Year-Project/Prototypes/TensorFlowObjectDetectionAPI_Prototype/models/research;C:/SourceCode/Final-Year-Project/Prototypes/TensorFlowObjectDetectionAPI_Prototype/models/research/slim
-python setup.py build
-python setup.py install
-python object_detection/builders/model_builder_test.py
